bc77b49e2de12da0ec81d85452fea16959bdbdb2
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-01-27 03:28:34 +0000

    rust: implement jfx

--- gradgen/templates/casadi-rs/lib.rs
+++ gradgen/templates/casadi-rs/lib.rs
@@ -3,10 +3,8 @@ use libc::{c_double, c_int};
 extern "C" {
     fn init_interface_{{name}}();
 
-    fn {{name}}_f(
-        arg: *const *const c_double,
-        casadi_results: *mut *mut c_double)
-        -> c_int;        
+    fn {{name}}_f(arg: *const *const c_double, casadi_results: *mut *mut c_double) -> c_int;        
+    fn {{name}}_jfx(arg: *const *const c_double, casadi_results: *mut *mut c_double) -> c_int;
 
 }
 
@@ -21,12 +19,13 @@ pub fn f(x: &[f64], u: &[f64], fxu: &mut [f64]) -> i32{
     let arguments = &[x.as_ptr(), u.as_ptr()];
     let result = &mut [fxu.as_mut_ptr()];
 
-    unsafe {
-        {{name}}_f(
-            arguments.as_ptr(),
-            result.as_mut_ptr()
-        ) as i32
-    }
+    unsafe { {{name}}_f(arguments.as_ptr(), result.as_mut_ptr()) as i32}
+}
+
+pub fn jfx(x: &[f64], u: &[f64], d: &[f64], jfxd: &mut [f64]) -> i32 {
+    let arguments = &[x.as_ptr(), u.as_ptr(), d.as_ptr()];
+    let result = &mut [jfxd.as_mut_ptr()];
+    unsafe { {{name}}_jfx(arguments.as_ptr(), result.as_mut_ptr()) as i32 }
 }
 
 #[cfg(test)]
@@ -45,5 +44,15 @@ mod tests {
         let mut fxu = [0.0; 3];
         assert_eq!(0, super::f(&x, &u, &mut fxu));
         println!("{:?}", fxu);
+    }
+    
+    #[test]
+    fn tst_jfx() {
+        let x = [0.1, 0.2, 0.3];
+        let u = [1.1, 2.2];
+        let d = [-0.8, 6.2, 0.0];
+        let mut jfx_d = [0.0; 3];
+        assert_eq!(0, super::jfx(&x, &u, &d, &mut jfx_d));
+        println!("{:?}", jfx_d);
     }
 }
