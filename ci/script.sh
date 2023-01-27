8bbd0961fff69b1c8059e2df894e2c71953ae590
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-01-27 03:33:08 +0000

    ci: first attempt
    - add workflow yml
    - add ci script

---
+++ ci/script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euxo pipefail
+
+
+regular_test() {
+    # Run Python tests
+    # ------------------------------------
+
+    # --- create virtual environment
+    cd gradgen
+    export PYTHONPATH=.
+
+    # --- install virtualenv
+    pip install virtualenv
+
+    # --- create virtualenv
+    virtualenv -p python3.8 venv
+
+    # --- activate venv
+    source venv/bin/activate
+
+    # --- upgrade pip within venv
+    pip install --upgrade pip
+
+    # --- install opengen
+    pip install .
+
+    # --- run the tests
+    export PYTHONPATH=.
+    python main.py
+    
+}
+
+main() {
+    regular_test
+}
+
+main
