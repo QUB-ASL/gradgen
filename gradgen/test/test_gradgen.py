089d08c65fe031861d0c0f3880d6e102c0c3e949
inLimonL <2623800166@qq.com>
2023-02-15 13:56:29 +0000

    update documentation

--- gradgen/test/test_gradgen.py
+++ gradgen/test/test_gradgen.py
@@ -1,10 +1,14 @@
 import os
 import unittest
 import casadi.casadi as cs
-from gradgen.cost_gradient import *
+from ..cost_gradient import *
 import logging
+
+
 import numpy as np
+
 import subprocess
+
 
 logger = logging.getLogger(__name__)
 
@@ -13,47 +17,31 @@ class GradgenTestCase(unittest.TestCase):
 
     @classmethod
     def create_example(cls):
-        nx, nu = 10, 3
-        k1, k2, Ts = 252, 20, 1 / 125
-        Gamma_u = np.diag([0, 0, 124.659])
-        Gamma_n = np.diag([2.287469, 1.35699886, -0.42388942])
-        I = np.diag([0.01788, 0.03014, 0.04614])
-        Iinv = np.linalg.inv(I)
+        nx, nu = 4, 1
+        m, I, g, ts= 1, 0.0005,9.81,0.01
+
         x = cs.SX.sym('x', nx)
         u = cs.SX.sym('u', nu)
-        (q0, q1, q2, q3, wx, wy, wz, nx, ny, nz) = (
-            x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9])
-        Q_bar_q = cs.vertcat(cs.vertcat(0, -x[5], -x[6], -x[7]).T,
-                             cs.vertcat(x[5], 0, x[7], -x[6]).T,
-                             cs.vertcat(x[6], -x[7], 0, x[5]).T,
-                             cs.vertcat(x[7], x[6], -x[5], 0).T)
-        tau = cs.sqrt(wx ** 2 + wy ** 2 + wz ** 2)
-        expQbar = cs.vertcat(
-            cs.vertcat(cs.cosh(tau), -wx * tau, -wy * tau, -wz * tau).T,
-            cs.vertcat(wx * tau, cs.cosh(tau), wz * tau, wy * tau).T,
-            cs.vertcat(wy * tau, -wz * tau, cs.cosh(tau), wx * tau).T,
-            cs.vertcat(wz * tau, wy * tau, -wx * tau, cs.cosh(tau)).T)
-        q = cs.vertcat(q0, q1, q2, q3)
-        w = cs.vertcat(wx, wy, wz)
-        n = cs.vertcat(nx, ny, nz)
-        f = cs.vertcat((Ts / 2) * expQbar @ q,
-                       w + Ts * (Gamma_n @ n + Gamma_u @ u -
-                                 Iinv @ cs.cross(w, I @ w)),
-                       n + Ts * (k1 * k2 * u - k2 * n)
-                       )
+
+        # System dynamics, f
+        f = cs.vertcat(
+            x[0] + ts * x[1],
+            x[1] + ts * ((5/7)*x[0]*x[3]**2-g * cs.sin(x[2])),
+            x[2] + ts * x[3],
+            x[3] + ts * ((u[0] - m * g * x[0] * cs.cos(x[2]) - 2 * m * x[0] * x[1] * x[2] ) / (m*x[0]**2+I)))
 
         # Stage cost function, ell
-        ell = 5*x[0]**2 + 0.01*x[1]**2 + 0.01*x[2]**2
+        ell = cs.dot(x, x) + cs.dot(u, u)
 
         # terminal cost function, vf
-        vf = 0.5 * (x[0]**2 + 50 * x[1]**2 + 100 * x[2]**2)
+        vf = 10 * cs.dot(x, x)
         return x, u, f, ell, vf
 
     def test_generate_code_and_build(self):
         x, u, f, ell, vf = GradgenTestCase.create_example()
         N = 15
         gradObj = CostGradient(x, u, f, ell, vf, N).with_name(
-            "quadcopter_test").with_target_path(".")
+            "ball_test").with_target_path("codegenz")
         gradObj.build(no_rust_build=True)
 
 
