2d624fcf86bb634628042c99e6bfd8158c02f5ca
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-03-20 23:16:35 +0000

    quadcopter in model factory

--- gradgen/model_factory.py
+++ gradgen/model_factory.py
@@ -33,9 +33,37 @@ class ModelFactory:
         return cs.simplify(x + t_sampling * f)
 
     @ staticmethod
-    def create_quadcopter_model(x, u):
+    def create_quadcopter_model(x, u,
+                                t_sampling=0.01,
+                                gamma_u_z=125.,
+                                gamma_n=[-2.3, -1.4, 0.4],
+                                moi=[0.02, 0.02, 0.05],
+                                k1=252, k2=20):
         _check_state_input_dims(x, u, 10, 3)
-        raise NotImplementedError()
+        q = x[0:4]
+        w = x[4:7]
+        n = x[7:]
+        norm_w = cs.norm_2(w)
+        tau = cs.sin(norm_w)/norm_w
+        wx_tau = w[0] * tau
+        wy_tau = w[1] * tau
+        wz_tau = w[2] * tau
+        csw = cs.cos(norm_w)
+        expQw = cs.vertcat(
+            cs.horzcat(csw, -wx_tau, -wy_tau, -wz_tau),
+            cs.horzcat(wx_tau, csw, wz_tau, -wx_tau),
+            cs.horzcat(wy_tau, -wz_tau, csw, wx_tau),
+            cs.horzcat(wz_tau, wy_tau, -wx_tau, csw)
+        )
+        fq = 0.5 * t_sampling * (expQw @ q)
+        gamma_u = np.diagflat([0, 0, gamma_u_z])
+        gamma_n = np.diagflat(gamma_n)
+        moi = np.diagflat(moi)
+        moi_inv = 1 / moi
+        fw = w + t_sampling * (gamma_u @ u + gamma_n @
+                               n - moi_inv @ cs.cross(w, moi @ w))
+        fn = n + t_sampling * (k1*k2*u - k2*n)
+        return cs.simplify(cs.vertcat(fq, fw, fn))
 
     @ staticmethod
     def create_ball_and_beam_model(x, u, m=1, g=9.81, moi=5e-4, t_sampling=0.01):
