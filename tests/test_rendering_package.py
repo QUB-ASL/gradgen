37df2204a2c69121ee3bf5dd72c8dd2d640c8fc3
Pantelis Sopasakis <p.sopasakis@gmail.com>
2026-04-05 13:27:49 +0100

    remove shallow unit tests

--- tests/test_rendering_package.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import gradgen._rust_codegen.rendering as rendering
-
-
-class RenderingPackageTests(unittest.TestCase):
-    def test_package_reexports_core_helpers(self) -> None:
-        self.assertTrue(hasattr(rendering, "_emit_node_expr"))
-        self.assertTrue(hasattr(rendering, "_build_shared_helper_lines"))
--- tests/test_rust_codegen_package.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import gradgen._rust_codegen as rust_codegen_package
-
-
-class RustCodegenPackageTests(unittest.TestCase):
-    def test_package_reexports_core_builder_types(self) -> None:
-        self.assertTrue(hasattr(rust_codegen_package, "CodeGenerationBuilder"))
-        self.assertTrue(hasattr(rust_codegen_package, "FunctionBundle"))
